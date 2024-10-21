10080eddb1af54ad82aa90ad92b632829ecb0468
anandcute <d.anandkumar2002@gmail.com>
2024-10-21 11:12:48 +0530

    commit

---
+++ SnakeGame/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Snake game = new Snake(6,6);
+        game.snakeMove(0,0);
+    }
+}
---
+++ SnakeGame/Node.java
@@ -0,0 +1,18 @@
+// package SnakeGame;
+
+public class Node {
+    private int row = 0 ;
+    private int col = 0 ;
+
+    Node(int row , int col){
+        this.row = row ;
+        this.col = col ;
+    }
+
+    public  int getRow(){
+        return row;
+    }
+    public  int getColumn(){
+        return col;
+    }
+}
